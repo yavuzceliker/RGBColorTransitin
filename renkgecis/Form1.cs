6a98d1de0abf84478903e59d541bebde6c0d1f55
Yavuz ÇELİKER <16008116016@ogr.bozok.edu.tr>
2018-03-28 14:34:02 +0300

    RGB Color Transition
    An implementation of all shades of RGB colors

---
+++ renkgecis/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace renkgecis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int r = 255,g=0,b=0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = "(" + r + "," + g + "," + b + ")";
+            this.BackColor = Color.FromArgb(r, g, b);
+            if (r == 255 && g < 255 && b==0)
+            {
+                g++;
+            }
+            else if (r > 0 && g == 255 && b == 0)
+            {
+                r--;
+            }
+            else if (r == 0 && g == 255 && b < 255)
+            {
+                b++;
+            }
+            else if (r == 0 && g >0 && b == 255)
+            {
+                g--;
+            }
+            else if (r < 255 && g == 0 && b == 255)
+            {
+                r++;
+            }
+            else if (r == 255 && g == 0 && b > 0)
+            {
+                b--;
+            }
+        }
+    }
+}
